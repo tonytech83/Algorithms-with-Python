90b6dcc7cd1a441b8a415f64430205b05a0e88a0
Anton Petrov <a.petrov.83@gmail.com>
2022-07-06 09:46:08 +0300

    Added exam "01 Recursive array sum" from "Recursion and Backtracking - Lab"

---
+++ 01_Recursion_and_Backtracking_Lab/01_recursive_array_sum.py
@@ -0,0 +1,11 @@
+def array_sum(numbers, index):
+    if index == len(numbers) - 1:  # Check is this is the bottom of our array
+        return numbers[index]  # if True, then return sum of last element of our array
+
+    return numbers[index] + array_sum(numbers, index + 1)  # sums the current element of aur array with rest of our array
+
+
+# Define the array ( example: 1 2 3 4 )
+numbers = [int(x) for x in input().split()]
+
+print(array_sum(numbers, 0))
