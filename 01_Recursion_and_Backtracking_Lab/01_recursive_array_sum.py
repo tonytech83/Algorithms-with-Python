66f6c052d00f853157ffd75d94dc9f0f67bc9993
Anton Petrov <a.petrov.83@gmail.com>
2022-07-10 15:55:10 +0300

    Added exam "07. Recursive Fibonacci" from "Recursion and Backtracking - Lab"

--- 01_Recursion_and_Backtracking_Lab/01_recursive_array_sum.py
+++ 01_Recursion_and_Backtracking_Lab/01_recursive_array_sum.py
@@ -1,8 +1,10 @@
 def array_sum(numbers, index):
-    if index == len(numbers) - 1:  # BASE CASE - Check is this is the bottom of our array
-        return numbers[index]  # if True, then return sum of last element of our array
-
-    return numbers[index] + array_sum(numbers, index + 1)  # sums the current element of aur array with rest of our array
+    # BASE CASE - Check is this is the bottom of our array
+    if index == len(numbers) - 1:
+        # if True, then return sum of last element of our array
+        return numbers[index]
+    # sums the current element of aur array with rest of our array
+    return numbers[index] + array_sum(numbers, index + 1)
 
 
 # Define the array ( example: 1 2 3 4 )
---
+++ 02_Recursion_and_Backtracking_Exercise/01_reverse_array.py
@@ -0,0 +1,17 @@
+def reversed_array(idx, numbers):
+    # base case - check for end on massive
+    if idx == len(numbers) // 2:
+        return
+
+    swap_idx = len(numbers) - 1 - idx
+    keep_number = numbers[idx]  # numbers[idx], numbers[swap_idx] = numbers[swap_idx], numbers[idx]
+    numbers[idx] = numbers[swap_idx]
+    numbers[swap_idx] = keep_number
+    # recursion
+    reversed_array(idx + 1, numbers)
+
+
+numbers = input().split()
+reversed_array(0, numbers)
+
+print(" ".join(numbers))
