fe7f37ea6e90055976c88e0e55fbdd39c6359df6
Anton Petrov <a.petrov.83@gmail.com>
2022-07-21 16:55:08 +0300

    Added exam "01. Shortest Path in Unweighted Graph" from "Graphs Shortest Path and MST - Lab"

---
+++ 06_Graphs_Shortest_Path_and_MST_Lab/01_shortest_path_in_unweighted_graph.py
@@ -0,0 +1,48 @@
+from collections import deque
+
+nodes = int(input())
+edges = int(input())
+
+graph = []
+[graph.append([]) for _ in range(nodes + 1)]
+
+for _ in range(edges):
+    source, destination = [int(x) for x in input().split()]
+    graph[source].append(destination)
+
+start_node = int(input())
+destination_node = int(input())
+
+visited = [False] * (nodes + 1)
+# contains parent node for each child node
+parent = [None] * (nodes + 1)
+
+# mark start_note as visited
+visited[start_node] = True
+
+# create queue
+queue = deque([start_node])
+
+while queue:
+    node = queue.popleft()
+    if node == destination_node:
+        break
+    for child in graph[node]:
+        if visited[child]:
+            continue
+        # if not visited, mark it as visited
+        visited[child] = True
+        # add it in queue
+        queue.append(child)
+        # mark current node as parent to current child
+        parent[child] = node
+
+path = deque()
+node = destination_node
+
+while node is not None:
+    path.appendleft(node)
+    node = parent[node]
+
+print(f"Shortest path length is: {len(path) - 1}")
+print(*path, sep=' ')
