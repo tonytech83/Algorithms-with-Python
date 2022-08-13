11f624de9b114ad8f32cbf2146efb5b5a094dc3e
Anton Petrov <a.petrov.83@gmail.com>
2022-08-13 19:43:43 +0300

    Added exam "1. Conditional Expression Resolver" from "Algorithms with Python Exam - 13 August 2022"

--- 11_Regular_Exam_13_August_2022/01_conditional_expression_resolver.py
+++ 11_Regular_Exam_13_August_2022/01_conditional_expression_resolver.py
@@ -1,1 +1,18 @@
-# TODO
+line_from_input = input()
+
+
+def resolve_expression(expression):
+    if '?' not in expression:
+        return expression
+
+    bool_val, choices = expression.split('?', 1)
+    true, false = choices.rsplit(':', 1)
+    bool_val = bool_val.strip()
+
+    if bool_val == 't':
+        return resolve_expression(true)
+    if bool_val == 'f':
+        return resolve_expression(false)
+
+
+print(resolve_expression(line_from_input))
--- 11_Regular_Exam_13_August_2022/03_arbitrage.py
+++ 11_Regular_Exam_13_August_2022/03_arbitrage.py
@@ -31,4 +31,46 @@ parent = [None] * (max_node + 1)
 
 price[start] = 0
 
-# TODO
+# from collections import deque
+#
+# trading_pairs = int(input())
+#
+# graph = {}
+# for _ in range(trading_pairs):
+#     source, target, rate = [x for x in input().split()]
+#     if source not in graph:
+#         graph[source] = []
+#         graph[source].append((target, float(rate)))
+#     else:
+#         graph[source].append((target, float(rate)))
+#
+# target_currency = input()
+#
+# path = [f'{target_currency}: 1.000']
+# path_currency = [target_currency]
+# current_val = 1
+#
+# queue = deque()
+# queue.append(target_currency)
+# visited = set()
+# while queue:
+#     current_cur = queue.popleft()
+#     visited.add(current_cur)
+#
+#     next_cur, rate = max(graph[current_cur], key=lambda i: i[1])
+#     current_val = current_val * rate
+#     path.append(f'{next_cur}: {current_val:.3f}')
+#     path_currency.append(next_cur)
+#
+#     # if next_cur in graph:
+#     if next_cur not in visited:
+#         queue.append(next_cur)
+#
+# ####
+# if current_val < 1:
+#     print("False")
+#     path.pop()
+#     print(*path, sep='\n')
+# elif current_val > 1:
+#     print("True")
+#     print(*path_currency, sep=' ')
