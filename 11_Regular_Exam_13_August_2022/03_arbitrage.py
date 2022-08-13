90ce04f765a8a1139e6fd466884a3a3a2c66f8ee
Anton Petrov <a.petrov.83@gmail.com>
2022-08-13 20:02:27 +0300

    Added exam "3. Arbitrage" from "Algorithms with Python Exam - 13 August 2022"

--- 11_Regular_Exam_13_August_2022/03_arbitrage.py
+++ 11_Regular_Exam_13_August_2022/03_arbitrage.py
@@ -1,12 +1,4 @@
-import sys
-
-
-class TradingPair:
-    def __init__(self, source, destination, price):
-        self.source = source
-        self.destination = destination
-        self.price = price
-
+from collections import deque
 
 trading_pairs = int(input())
 
@@ -16,61 +8,36 @@ for _ in range(trading_pairs):
     source, destination, price = [str(x) for x in input().split(' ')]
     if source not in graph:
         graph[source] = []
-    if destination not in graph:
-        graph[destination] = []
-    graph[source].append(TradingPair(source, destination, price))
+        graph[source].append((destination, float(price)))
+    else:
+        graph[source].append((destination, float(price)))
 
-start = input()
-end = start
+currency = input()
 
-max_node = max(graph.keys())
+path = [f'{currency}: 1.000']
+path_currency = [currency]
+current_value = 1
 
-price = [sys.maxsize] * (max_node + 1)
-# create parent array
-parent = [None] * (max_node + 1)
+queue = deque()
+queue.append(currency)
+visited = set()
 
-price[start] = 0
+while queue:
+    current_currency = queue.popleft()
+    visited.add(current_currency)
 
-# from collections import deque
-#
-# trading_pairs = int(input())
-#
-# graph = {}
-# for _ in range(trading_pairs):
-#     source, target, rate = [x for x in input().split()]
-#     if source not in graph:
-#         graph[source] = []
-#         graph[source].append((target, float(rate)))
-#     else:
-#         graph[source].append((target, float(rate)))
-#
-# target_currency = input()
-#
-# path = [f'{target_currency}: 1.000']
-# path_currency = [target_currency]
-# current_val = 1
-#
-# queue = deque()
-# queue.append(target_currency)
-# visited = set()
-# while queue:
-#     current_cur = queue.popleft()
-#     visited.add(current_cur)
-#
-#     next_cur, rate = max(graph[current_cur], key=lambda i: i[1])
-#     current_val = current_val * rate
-#     path.append(f'{next_cur}: {current_val:.3f}')
-#     path_currency.append(next_cur)
-#
-#     # if next_cur in graph:
-#     if next_cur not in visited:
-#         queue.append(next_cur)
-#
-# ####
-# if current_val < 1:
-#     print("False")
-#     path.pop()
-#     print(*path, sep='\n')
-# elif current_val > 1:
-#     print("True")
-#     print(*path_currency, sep=' ')
+    next_currency, price = max(graph[current_currency], key=lambda i: i[1])
+    current_value = current_value * price
+    path.append(f'{next_currency}: {current_value:.3f}')
+    path_currency.append(next_currency)
+
+    if next_currency not in visited:
+        queue.append(next_currency)
+
+if current_value < 1:
+    print("False")
+    path.pop()
+    print(*path, sep='\n')
+elif current_value > 1:
+    print("True")
+    print(*path_currency, sep=' ')
