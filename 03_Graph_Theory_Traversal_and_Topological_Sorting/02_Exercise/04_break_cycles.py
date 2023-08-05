3730a91db41ea1b918fb31f2b02a2fb595ac19ab
Anton Petrov <a.petrov.83@gmail.com>
2023-08-05 10:12:52 +0300

    added solution with DFS

--- 03_Graph_Theory_Traversal_and_Topological_Sorting/02_Exercise/04_break_cycles.py
+++ 03_Graph_Theory_Traversal_and_Topological_Sorting/02_Exercise/04_break_cycles.py
@@ -1,3 +1,11 @@
+# exam: 04. Break Cycles
+# judge: https://judge.softuni.org/Contests/Compete/Index/3463#3
+
+"""
+Solution with DFS
+"""
+
+
 def dfs(node, destination, graph, visited):
     if node in visited:
         return
@@ -20,37 +28,34 @@ def path_exists(source, destination, graph):
 
 
 nodes = int(input())
-
 graph = {}
 
 # create array where to store all edges
 # for example if we have: 1 -> 2 5 4
 # we will store in edges array (1, 2), (1, 5), (1, 4)
 edges = []
-
 for _ in range(nodes):
-    node, children_str = input().split(" -> ")
-    children = children_str.split()
-    graph[node] = children
-    for child in children:
+    node, childs = input().split(' -> ')
+    graph[node] = [x for x in childs.split()]
+    for child in childs.split():
         edges.append((node, child))
 
-# keep removed edges
 removed_edges = []
 
-for source, destination in sorted(edges, key=lambda x: (x[0], x[1])):
+for source, destination in sorted(edges, key=lambda t: (t[0], t[1])):
     if destination not in graph[source] or source not in graph[destination]:
         continue
+
+    # removes edges between source and destination
     graph[source].remove(destination)
     graph[destination].remove(source)
 
     if path_exists(source, destination, graph):
         removed_edges.append((source, destination))
-
     else:
+        # if path not exist returns the edges
         graph[source].append(destination)
         graph[destination].append(source)
 
 print(f'Edges to remove: {len(removed_edges)}')
-for first, second in removed_edges:
-    print(f"{first} - {second}")
+[print(f'{source} - {destination}') for source, destination in removed_edges]
