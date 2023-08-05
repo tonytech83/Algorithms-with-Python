f0d9abd254fe82b49bc69135cf49e643b2e1a8da
Anton Petrov <a.petrov.83@gmail.com>
2023-08-05 20:10:09 +0300

    added example of Kruskal's Algorithm

--- 04_Graphs_Shortest_Path_and_MST/01_Lab/04_kruskal_algorithm.py
+++ 04_Graphs_Shortest_Path_and_MST/01_Lab/04_kruskal_algorithm.py
@@ -1,7 +1,9 @@
-def find_root(parent, node):
-    while node != parent[node]:
-        node = parent[node]
-    return node
+"""
+exam: 04. Kruskal's Algorithm
+judge: https://judge.softuni.org/Contests/Compete/Index/3464#3
+
+Minimum Spanning Tree (MST)
+"""
 
 
 class Edge:
@@ -9,6 +11,14 @@ class Edge:
         self.first = first
         self.second = second
         self.weight = weight
+
+
+def find_root(parent, node):
+    while node != parent[node]:
+        node = parent[node]
+
+    # return root when node is root for him self
+    return node
 
 
 edges = int(input())
@@ -20,20 +30,23 @@ max_node = float('-inf')
 for _ in range(edges):
     first, second, weight = [int(x) for x in input().split(', ')]
     graph.append(Edge(first, second, weight))
-    # take the max node
     max_node = max(first, second, max_node)
 
 # create parent array where initially all nodes has themselves as root
-parent = [num for num in range(max_node + 1)]
+# node (idx)    0 1 2 3 4 ... max_node
+# parent        0 1 2 3 4 ... max_node
+parent = [n for n in range(max_node + 1)]
 
+# create array to store edges
 forest = []
 
 for edge in sorted(graph, key=lambda e: e.weight):
     first_node_root = find_root(parent, edge.first)
     second_node_root = find_root(parent, edge.second)
+
+    # if True, attaches first_root to second_root
     if first_node_root != second_node_root:
         parent[first_node_root] = second_node_root
         forest.append(edge)
 
-for edge in forest:
-    print(f"{edge.first} - {edge.second}")
+[print(f'{edge.first} - {edge.second}') for edge in forest]
