d1d06e141d7051c194667204ca69c117fe4aaa0c
Anton Petrov <a.petrov.83@gmail.com>
2023-08-05 17:01:03 +0300

    added example of Bellman-Ford Algorithm

--- 04_Graphs_Shortest_Path_and_MST/01_Lab/03_bellman-ford.py
+++ 04_Graphs_Shortest_Path_and_MST/01_Lab/03_bellman-ford.py
@@ -1,11 +1,19 @@
+"""
+exam: 03. Bellman-Ford
+judge: https://judge.softuni.org/Contests/Compete/Index/3464#2
+
+Computes shortest paths from a source node to all the other nodes in a weighted graph.
+- weights on edges can be negative
+- can detect and report negative cycles
+"""
 from collections import deque
 
 
 class Edge:
     def __init__(self, source, destination, weight):
-        self.weight = weight
-        self.destination = destination
         self.source = source
+        self.destination = destination
+        self.weight = weight
 
 
 nodes = int(input())
@@ -18,27 +26,34 @@ for _ in range(edges):
     graph.append(Edge(source, destination, weight))
 
 start = int(input())
-end = int(input())
+target = int(input())
 
-# create two arrays for distance and parents
-distance = [float("inf")] * (nodes + 1)
+# create two arrays for distances and parents
+distance = [float('inf')] * (nodes + 1)
 parent = [None] * (nodes + 1)
 
 distance[start] = 0
 
 for _ in range(nodes - 1):
     updated = False
+
+    # iterate through all edges
     for edge in graph:
         if distance[edge.source] == float('inf'):
             continue
+
         new_distance = distance[edge.source] + edge.weight
+        # if True updates destination distance and parent
         if new_distance < distance[edge.destination]:
             distance[edge.destination] = new_distance
             parent[edge.destination] = edge.source
             updated = True
+
+    # if False stop iteration because we don't have any updates on distances
     if not updated:
         break
 
+# detection of negative cycle
 for edge in graph:
     new_distance = distance[edge.source] + edge.weight
     if new_distance < distance[edge.destination]:
@@ -46,11 +61,11 @@ for edge in graph:
         break
 else:
     path = deque()
-    node = end
+    node = target
 
     while node is not None:
         path.appendleft(node)
         node = parent[node]
 
     print(*path, sep=' ')
-    print(distance[end])
+    print(distance[target])
