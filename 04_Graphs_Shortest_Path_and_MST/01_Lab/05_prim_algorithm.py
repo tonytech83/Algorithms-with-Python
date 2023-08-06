fe985ef93c53d57fc5f2fb2fbe7f9ec2a8c77f35
Anton Petrov <a.petrov.83@gmail.com>
2023-08-06 08:54:05 +0300

    Added example of Prim's Algorithm

--- 04_Graphs_Shortest_Path_and_MST/01_Lab/05_prim_algorithm.py
+++ 04_Graphs_Shortest_Path_and_MST/01_Lab/05_prim_algorithm.py
@@ -1,3 +1,15 @@
+"""
+exam: 05. Prim's Algorithm
+judge: https://judge.softuni.org/Contests/Compete/Index/3464#3
+
+Minimum Spanning Tree (MST) with Prim's algorithm using PriorityQueue
+- Given a graph G(V, E) find the minimum spanning forest T(V', E')
+- Attach to the tree T the starting node
+- While smallest edge exists
+    - Attach to T the smallest possible edge from G without creating a cycle in T
+        - Use the smallest edge (u, v),such that u ∈ T and v ∉ T
+- Start the Prim's algorithm for each node from G
+"""
 from queue import PriorityQueue
 
 
@@ -7,7 +19,6 @@ class Edge:
         self.second = second
         self.weight = weight
 
-    # sorting by weight
     def __gt__(self, other):
         return self.weight > other.weight
 
@@ -17,19 +28,21 @@ def prim(node, graph, forest, forest_edges):
     forest.add(node)
 
     pq = PriorityQueue()
-    for edge in graph[node]:
-        pq.put(edge)
+
+    for current_edge in graph[node]:
+        pq.put(current_edge)
 
     while not pq.empty():
         min_edge = pq.get()
-        non_tree_node = - 1
+        non_tree_node = None
 
         if min_edge.first in forest and min_edge.second not in forest:
             non_tree_node = min_edge.second
-        elif min_edge.second in forest and min_edge.first not in forest:
+
+        if min_edge.first not in forest and min_edge.secong in forest:
             non_tree_node = min_edge.first
 
-        if non_tree_node == -1:
+        if non_tree_node is None:
             continue
 
         forest.add(non_tree_node)
@@ -45,8 +58,10 @@ graph = {}
 
 for _ in range(edges):
     first, second, weight = [int(x) for x in input().split(', ')]
+
     if first not in graph:
         graph[first] = []
+
     if second not in graph:
         graph[second] = []
 
@@ -60,7 +75,7 @@ forest_edges = []
 for node in graph:
     if node in forest:
         continue
+
     prim(node, graph, forest, forest_edges)
 
-for edge in forest_edges:
-    print(f"{edge.first} - {edge.second}")
+[print(f'{edge.first} - {edge.second}') for edge in forest_edges]
