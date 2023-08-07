eaed172bae85e7bb938fb3448be6a5a2494819c6
Anton Petrov <a.petrov.83@gmail.com>
2023-08-07 09:55:40 +0300

    added example of fibonacci problem with Recursive approach using dynamic programming (memorization)

--- 05_Dynamic_Programming/01_Lab/01_fibonacci.py
+++ 05_Dynamic_Programming/01_Lab/01_fibonacci.py
@@ -1,15 +1,24 @@
+"""
+exam: 01. Fibonacci
+judge: https://judge.softuni.org/Contests/Practice/Index/3471#0
+
+Recursive approach with dynamic programming (memorization)
+"""
+
+
 def calc_fib(n, memo):
     # check if we already had n in dict memo
     if n in memo:
         return memo[n]
-    # check if we already hit the bottom
+
+    # BASE CASE - bottom of recursion
     if n <= 2:
         return 1
 
-    # if calc is not i our dict memo
+    # calc the fibonacci of n and add result in memo dict
     result = calc_fib(n - 1, memo) + calc_fib(n - 2, memo)
-    # caching calculation, to have less calculation
     memo[n] = result
+
     return result
 
 
